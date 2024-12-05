a67a98267433ddde6c9edd2b98e188bedb9464a7
itspalmera <88901304+itspalmera@users.noreply.github.com>
2024-12-05 18:11:42 -0400

    feat: add parent folders

---
+++ Src/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GYM_Body_Light_API.Src.Models
+{
+    public class User
+    {
+        
+    }
+}
