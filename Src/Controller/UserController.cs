1154c9a5634e7bf55e5f754459ac6d5c2e3973c9
itspalmera <88901304+itspalmera@users.noreply.github.com>
2024-12-05 18:17:18 -0400

    feat: add parent folders

---
+++ Src/Controller/UserController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GYM_Body_Light_API.Src.Controller
+{
+    public class UserController
+    {
+        
+    }
+}
---
+++ Src/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GYM_Body_Light_API.Src.DTOs
+{
+    public class UserDto
+    {
+        
+    }
+}
